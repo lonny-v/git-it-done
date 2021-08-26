d49a012e6829abf55697c5b55fab07b585cde067
Lonny Vberg <lonnyvandenberg@gmail.com>
2021-08-26 00:34:10 -0600

    add error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,16 +4,31 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var getUserRepos = function (user) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function (response) {
-        console.log(response);
-        response.json().then(function (data) {
-            displayRepos(data, user);
+    fetch(apiUrl)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert('Error: GitHub User Not Found');
+            }
+        })
+        .catch(function (error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
         });
-    });
 };
 
 var formSubmitHandler = function (event) {
