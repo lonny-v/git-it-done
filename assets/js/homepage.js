46a28a421198c98aef2e949c15c1372c14bf8fdf
Lonny Vberg <lonnyvandenberg@gmail.com>
2021-08-25 23:57:36 -0600

    form submission, able to search for users in search bar

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -11,4 +11,22 @@ var getUserRepos = function (user) {
     });
 };
 
-getUserRepos("lonny-v");
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
